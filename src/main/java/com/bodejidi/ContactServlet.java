ca6eef88fbea6b119fc161b25083d039b039ffb5
lidgik <dbwen123@gmail.com>
2014-07-20 02:37:59 +0800

    添加了最基本的查询，并且释放所有资源

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -6,10 +6,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ContactServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 		throws IOException, ServletException {
-		response.getWriter().println("contacts");
+		response.getWriter().println("get contact by id:" + request.getParameter("contactId"));
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch(Exception e){
+			// handle the error
+		}
+		
+		Connection connection = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try{
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery("select * from contact where id=1");
+			rs.next();
+			response.getWriter().println(rs.getString("name"));
+		} catch(SQLException sqle){
+			sqle.printStackTrace();
+		}
+
+		if(rs != null){
+			try{
+				rs.close();
+			} catch(Exception e){
+				// handle the error
+			}
+		}
+		
+		if(stmt != null){
+			try{
+				stmt.close();
+			} catch(Exception e){
+				// handle the error
+			}
+		}
+		
+		if(connection != null){
+			try{
+				connection.close();
+			} catch(Exception e){
+				// handle the error
+			}
+		}
 	}
 	
 }
