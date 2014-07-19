d62b53bd4a984cb53e0262f53a4af24be9b8a7ab
lidgik <dbwen123@gmail.com>
2014-07-20 03:02:23 +0800

    查询所有的contact

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -15,52 +15,102 @@ import java.sql.Statement;
 public class ContactServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 		throws IOException, ServletException {
-		response.getWriter().println("get contact by id:" + request.getParameter("contactId"));
-		
-		try{
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch(Exception e){
-			// handle the error
-		}
-		
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		try{
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("select * from contact where id=" + request.getParameter("contactId"));
-			if(rs.next()){
-				response.getWriter().println("Name:" + rs.getString("name"));
-			} else {
-				response.getWriter().println("Contact not found.");
-			}	
-		} catch(SQLException sqle){
-			sqle.printStackTrace();
-		}
-
-		if(rs != null){
+		if(request.getParameter("contactId") == null){
+			response.getWriter().println("Get all contacts.");
+			
 			try{
-				rs.close();
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
 			} catch(Exception e){
 				// handle the error
 			}
-		}
 		
-		if(stmt != null){
+			Connection connection = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+
 			try{
-				stmt.close();
+				connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
+				stmt = connection.createStatement();
+				rs = stmt.executeQuery("select * from contact");
+				while(rs.next()){
+					response.getWriter().println("Name:" + rs.getString("name"));
+				}
+			} catch(SQLException sqle){
+				sqle.printStackTrace();
+			}
+
+			if(rs != null){
+				try{
+					rs.close();
+				} catch(Exception e){
+					// handle the error
+				}
+			}
+		
+			if(stmt != null){
+				try{
+					stmt.close();
+				} catch(Exception e){
+					// handle the error
+				}
+			}
+		
+			if(connection != null){
+				try{
+					connection.close();
+				} catch(Exception e){
+					// handle the error
+				}
+			}
+
+		} else{
+			response.getWriter().println("get contact by id:" + request.getParameter("contactId"));
+			
+			try{
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
 			} catch(Exception e){
 				// handle the error
 			}
-		}
 		
-		if(connection != null){
+			Connection connection = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+
 			try{
-				connection.close();
-			} catch(Exception e){
-				// handle the error
+				connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
+				stmt = connection.createStatement();
+				rs = stmt.executeQuery("select * from contact where id=" + request.getParameter("contactId"));
+				if(rs.next()){
+					response.getWriter().println("Name:" + rs.getString("name"));
+				} else {
+					response.getWriter().println("Contact not found.");
+				}	
+			} catch(SQLException sqle){
+				sqle.printStackTrace();
+			}
+
+			if(rs != null){
+				try{
+					rs.close();
+				} catch(Exception e){
+					// handle the error
+				}
+			}
+		
+			if(stmt != null){
+				try{
+					stmt.close();
+				} catch(Exception e){
+					// handle the error
+				}
+			}
+		
+			if(connection != null){
+				try{
+					connection.close();
+				} catch(Exception e){
+					// handle the error
+				}
 			}
 		}
 	}
